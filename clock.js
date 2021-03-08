b7d4449077d78b2cddc2f7df5599f0aa5573eb08
simata98 <wlsrnr905@naver.com>
2021-03-08 20:32:16 +0900

    added now on time and '00~09' seconds

--- clock.js
+++ clock.js
@@ -6,11 +6,18 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  // '?' means "if it is true" ':' means " if it is not true"
+  // I did this beacause I can watch seconds "00 ~ 09"
+  clockTitle.innerText = `${hours}:${minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
 }
 
 function init() {
   getTime();
+  setInterval(getTime, 1000); // I can update every 1 sec.
 }
 
 init();
+
+// clock is working! but it isn't now on time...
